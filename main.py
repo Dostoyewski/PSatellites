489eab8f116e261411fc1d2be5843aa28a7b95ef
Dostoyewski <dostoyewski@yandex.ru>
2021-03-18 12:24:24 +0300

    First decoder version

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+from decoder import decode
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    decode('sample_data/_2207_noaa17.wav')
